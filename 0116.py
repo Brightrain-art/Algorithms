0e8f091afa9cce2b14e746f6a639d50ef7d98608
balbi.hw <brightrain453@gmail.com>
2026-01-16 08:54:37 +0900

    0116

---
+++ 0116.py
@@ -0,0 +1,28 @@
+A, B = map(int, input().split())
+fir = []
+sco = []
+
+for d in range(2, A):
+    if A % d == 0:
+        fir.append(d)
+for i in range(2, B):
+    if B % i == 0:
+        sco.append(i)
+
+maxi = []
+
+for a in fir:
+    if a in sco:
+        maxi.append(a)
+
+print(max(maxi))
+
+mul = []
+for b in range(1, B+1):
+    aa = A*b
+    for c in range(1, A*1):
+        bb = B*c
+        if aa == bb:
+            mul.append(aa)
+
+print(min(mul))
